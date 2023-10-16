e840ec4221c37f8aabba3ba171d6177d952761a0
PAZHANI123 <119094119+PAZHANI123@users.noreply.github.com>
2023-10-16 10:04:28 +0530

    Add files via upload

---
+++ Day 21/src/main/java/magajan/college_admission_portal/CollegeAdmissionPortalApplication.java
@@ -0,0 +1,13 @@
+package magajan.college_admission_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeAdmissionPortalApplication {
+;;
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeAdmissionPortalApplication.class, args);
+	}
+
+}i
---
+++ Day 21/src/test/java/magajan/college_admission_portal/CollegeAdmissionPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package magajan.college_admission_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeAdmissionPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
